d6d09719d094f089600281a21e8baaef8854c856
Lester063 <102047666+Lester063@users.noreply.github.com>
2024-01-07 06:13:18 -0800

    add toggle navbar
    add toggle navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -98,6 +98,9 @@ const Navbar = () => {
         menu = (
             <>
                 <Link className="navbar-brand" to="/">Bowie</Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li className="nav-item">
@@ -123,6 +126,9 @@ const Navbar = () => {
         menu = (
             <>
                 <Link className="navbar-brand" to="/">{name}</Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li className="nav-item">
