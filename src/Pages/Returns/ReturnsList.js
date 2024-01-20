2a77f882b2774f96b68d3905d5bf739b229f108f
Lester063 <102047666+Lester063@users.noreply.github.com>
2024-01-20 00:39:33 -0800

    refactor Returns pages
    refactor returns pages, and fixed notif

--- src/Pages/Returns/ReturnsList.js
+++ src/Pages/Returns/ReturnsList.js
@@ -1,11 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect, useContext } from "react";
 import HoverMessage from "../../components/HoverMessage";
 import { useLocation } from "react-router-dom";
+import { ViewReturnContext } from "./ViewReturn";
+import { MyReturnContext } from "./MyReturn";
+import { UsersReturnContext } from "./UsersReturn";
+import axios from 'axios';
+import io from 'socket.io-client';
 
-const ReturnsList = ({ returns, approveReturn }) => {
+const socket = io.connect('http://localhost:3001');
+const ReturnsList = () => {
     const is_admin = localStorage.getItem('is_admin');
     const [message, setMessage] = useState('');
     const url = useLocation();
+
+    const viewreturndata = useContext(ViewReturnContext);
+    const myreturndata = useContext(MyReturnContext);
+    const usersreturndata = useContext(UsersReturnContext);
+    const [returns, setReturns] = useState([]);
+
+    useEffect(()=>{
+        setReturns(viewreturndata);
+    },[viewreturndata]);
+
+    useEffect(()=>{
+        setReturns(myreturndata);
+    },[myreturndata]);
+
+    useEffect(()=>{
+        setReturns(usersreturndata);
+    },[usersreturndata]);
 
     const hoverDisabledButton = async (id, isDisabled) => {
         if (isDisabled) {
@@ -17,6 +40,55 @@ const ReturnsList = ({ returns, approveReturn }) => {
     const hoverOut = async (id) => {
         document.getElementById(id).style.display = 'none';
     }
+
+    const approveReturn = async (e, returnid) => {
+        e.preventDefault();
+        let data = 'data,not sure why I need to pass a data, definitely a bug.'
+        try {
+            let response = await axios.put(`http://localhost:8000/api/return/${returnid}/approve`, data, { withCredentials: true });
+            let updatedReturn;
+            if (response.status === 200) {
+                const newReturns = returns.map((returnn) => {
+                    if (String(returnn.id) === String(returnid)) {
+                        updatedReturn = {
+                            ...returnn,
+                            is_approve: 1
+                        }
+                        socket.emit("sendNotificationToServer", [response.data.notification, updatedReturn, 'return']);
+                        return updatedReturn;
+                    }
+                    return returnn;
+                });
+                setReturns(newReturns);
+            }
+            else {
+                console.log(response.status);
+            }
+        }
+        catch (error) {
+            alert(error.response.data.message);
+        }
+    }
+
+    useEffect(()=>{
+        socket.on("sendNotificationToClient", ([message, updatedReturn, action]) => {
+            if(action === 'return') {
+                const newreturn = returns.map((returnn)=>{
+                    if(String(returnn.id) === String(updatedReturn.id)) {
+                        const updated = {
+                            ...returnn,
+                            is_approve: updatedReturn.is_approve,
+                        };
+                        return updated;
+                    }
+                    return returnn;
+                });
+                setReturns(newreturn);
+            }
+        });
+
+        return () => socket.off('sendNotificationToClient');
+    },[socket, returns]);
 
     return (
         <table className="table table-striped">
