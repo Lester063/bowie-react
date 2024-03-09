787e597fd358af2412c2a44de3aa264dfb3d7713
Lester063 <102047666+Lester063@users.noreply.github.com>
2024-03-09 01:18:01 -0800

    fixed login, register, home loading, profile
    fixed login, register, home loading, profile

--- src/Pages/Auth/Register.js
+++ src/Pages/Auth/Register.js
@@ -7,22 +7,22 @@ const Register = () => {
     const [inputError, setInputError] = useState({});
 
     const [user, setUser] = useState({
-        name:"",
-        email:"",
-        password:"",
+        name: "",
+        email: "",
+        password: "",
     });
 
-    useEffect(()=>{
+    useEffect(() => {
         setLoading(true);
-        setTimeout(()=>{
+        setTimeout(() => {
             setLoading(false);
-        },200);
-    },[])
+        }, 200);
+    }, [])
 
     //input changes
     const handleChange = (e) => {
         e.preventDefault();
-        setUser({...user, [e.target.name]: e.target.value});
+        setUser({ ...user, [e.target.name]: e.target.value });
     }
 
     const saveUser = (e) => {
@@ -38,18 +38,18 @@ const Register = () => {
             setLoading(false);
             alert(res.data.message);
             setUser({
-                name:"",
-                email:"",
-                password:""
+                name: "",
+                email: "",
+                password: ""
             })
             setInputError({});
-        }).catch(function(error){
-            if(error.response) {
-                if(error.response.status === 422) {
+        }).catch(function (error) {
+            if (error.response) {
+                if (error.response.status === 422) {
                     setLoading(false);
                     setInputError(error.response.data.errors);
                 }
-                else if(error.response.status === 500) {
+                else if (error.response.status === 500) {
                     setLoading(false);
                     setInputError(error.response.data.message);
                 }
@@ -61,19 +61,23 @@ const Register = () => {
         <div className="mobile-body">
             {loading && <Loading />}
             {!loading &&
-                <form onSubmit={saveUser}>
-                    <h1>Please register</h1>
-                    <input type="text" placeholder="Name" className="form-control" value={user.name} name="name" onChange={handleChange}/>
-                    <span className="text-danger">{inputError.name}</span>
-                    <input type="email" placeholder="Email" className="form-control" value={user.email} name="email" onChange={handleChange} />
-                    <span className="text-danger">{inputError.email}</span>
-                    <input type="password" placeholder="Password" className="form-control" value={user.password} name="password" onChange={handleChange} />
-                    <span className="text-danger">{inputError.password}</span>
-                    <button className="w-100 btn btn-lg btn-primary">Register</button>
-                </form>
+                <div class="row">
+                    <div className={window.innerWidth < 700 ? "col-11 mt-3 mx-auto" : "col-3 mt-3 mx-auto"}>
+                    <form onSubmit={saveUser}>
+                        <h1>Please register</h1>
+                        <input type="text" placeholder="Name" className="form-control mt-1" value={user.name} name="name" onChange={handleChange} />
+                        <span className="text-danger">{inputError.name}</span>
+                        <input type="email" placeholder="Email" className="form-control mt-1" value={user.email} name="email" onChange={handleChange} />
+                        <span className="text-danger">{inputError.email}</span>
+                        <input type="password" placeholder="Password" className="form-control mt-1" value={user.password} name="password" onChange={handleChange} />
+                        <span className="text-danger">{inputError.password}</span>
+                        <button className="w-100 btn btn-lg btn-primary mt-2">Register</button>
+                    </form>
+                    </div>
+                </div>
             }
         </div>
     );
 }
- 
+
 export default Register;
--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -10,89 +10,99 @@ const Profile = () => {
     const [userData, setUserData] = useState([]);
     const [profileImage, setProfileImage] = useState(null);
     const userid = localStorage.getItem('userid');
-    async function getUser() {
+
+    async function getUserData() {
         try {
-            if(userid !== null) {
+            if (userid !== null) {
                 const response = await axios.get('http://localhost:8000/api/user', { withCredentials: true });
                 setUserData(response.data);
-                getProfile(response.data)
+                getUserProfile(response.data)
             }
         }
-        catch(error) {
+        catch (error) {
             console.log('error:');
         }
     }
 
-    async function getProfile(datas) {
+    async function getUserProfile(user) {
         try {
-            datas.profile_image !== null ?
-            setProfileImage(`http://localhost:8000/storage/${datas.profile_image}`)
-            :
-            setProfileImage(null);
+            user.profile_image !== null ?
+                setProfileImage(`http://localhost:8000/storage/${user.profile_image}`)
+                :
+                setProfileImage(null);
         }
-        catch(error) {
+        catch (error) {
             console.log('error: ');
             console.log(error);
         }
     }
 
-    useEffect(()=>{
-        getUser();
-    },[]);
+    useEffect(() => {
+        getUserData();
+    }, []);
 
     let menu;
-    {userid === null ?
-        menu = (
-            <ForbiddenPage/>
-        ) 
-        :
-        menu = (
-            <>
-            {loading &&
-                <Loading />
-            }
-            {!loading && userData && 
-            <div className="container">
-            <div className="row">
-                <div className="col md-12">
-                    <div className="card mt-3">
-                        <div className="card-header">
-                            <h1>Personal Information</h1>
+    {
+        userid === null ?
+            menu = (
+                <ForbiddenPage />
+            )
+            :
+            menu = (
+                <>
+                    {loading &&
+                        <Loading />
+                    }
+                    {!loading && userData &&
+                        <div className="container">
+                            <div className="row">
+                                <div className="col md-12">
+                                    <div className="card mt-3">
+                                        <div className="card-header">
+                                            <h1>Personal Information</h1>
+                                        </div>
+                                        <div className="card-body">
+                                            <table className="table table-striped">
+                                                <tbody>
+                                                    <tr>
+                                                        <td style={{ width: "200px" }}>Profile Picture:<br/>
+                                                            <input type="file" name="uploadfile" id="img" style={{display:"none"}} />
+                                                            <label for="img"><i class="bi bi-camera"></i></label>
+                                                        </td>
+                                                        <td>{profileImage !== null
+                                                            ? <img src={profileImage} style={{ width: "250px" }} />
+                                                            :
+                                                            <>Please upload your profile picture.</>}
+                                                        </td>
+
+                                                    </tr>
+                                                    <tr>
+                                                        <td style={{ width: "200px" }}>First Name: </td>
+                                                        <td>{userData.name}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td style={{ width: "200px" }}>Middle Name: </td>
+                                                        <td>Middle Name</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td style={{ width: "200px" }}>Last Name: </td>
+                                                        <td>Last Name</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="card-body">
-                            <table className="table table-striped">
-                                <tbody>
-                                    <tr>
-                                        <td style={{width: "200px"}}>Profile Picture: </td>
-                                        <td>{profileImage !== null ? <img src={profileImage} style={{width:"250px"}}/> : <>Please upload your profile picture.</>}</td>
-                                    </tr>
-                                    <tr>
-                                        <td style={{width: "200px"}}>First Name: </td>
-                                        <td>{userData.name}</td>
-                                    </tr>
-                                    <tr>
-                                        <td style={{width: "200px"}}>Middle Name: </td>
-                                        <td>Middle Name</td>
-                                    </tr>
-                                    <tr>
-                                        <td style={{width: "200px"}}>Last Name: </td>
-                                        <td>Last Name</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-            }
-            </>
-        )
+                    }
+                </>
+            )
     }
     return (
         <div className="mobile-body">
             {menu}
-            
+
         </div>
     );
 }
