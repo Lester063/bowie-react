534d34ea89fb34c49081b21e1456a2c9f15309f4
Lester063 <102047666+Lester063@users.noreply.github.com>
2024-03-30 02:41:49 -0700

    display reviews
    added fetchDataItemReview to fetch item reviews from the backend

--- src/Pages/Item/ItemShow.js
+++ src/Pages/Item/ItemShow.js
@@ -7,8 +7,9 @@ const ItemShow = () => {
     const [loading, setLoading] = useState(true);
     const { id } = useParams();
     const [item, setItem] = useState([]);
+    const [itemReviews, setItemReview] = useState([]);
 
-    const fetchData = async () => {
+    const fetchDataItem = async () => {
         try {
             const response = await axios.get(`http://localhost:8000/api/items/${id}`, { withCredentials: true });
             setItem(response.data.data);
@@ -17,9 +18,21 @@ const ItemShow = () => {
             console.log(error);
         }
     }
+
+    const fetchDataItemReview = async () => {
+        try {
+            const response = await axios.get(`http://localhost:8000/api/itemreviews/${id}`, { withCredentials: true });
+            setItemReview(response.data.data);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
     useEffect(() => {
         document.title = 'Item';
-        fetchData();
+        fetchDataItem();
+        fetchDataItemReview();
         setLoading(false);
     }, []);
 
@@ -28,15 +41,15 @@ const ItemShow = () => {
             {loading && <Loading />}
             <div className="row mt-3">
                 <h3>{item.itemname}</h3>
-                <div className="col-6" style={{maxHeight:"400px"}}>
+                <div className="col-6" style={{ maxHeight: "400px" }}>
                     {item &&
                         <>
-                            {item.item_image ? 
-                                <img src={`http://localhost:8000/storage/${item.item_image}`} alt="Item Image" 
-                                style={{
-                                    maxHeight:"100%",
-                                    maxWidth: "100%"
-                                }} 
+                            {item.item_image ?
+                                <img src={`http://localhost:8000/storage/${item.item_image}`} alt="Item Image"
+                                    style={{
+                                        maxHeight: "100%",
+                                        maxWidth: "100%"
+                                    }}
                                 />
                                 :
                                 <p>No image available for this item.</p>
@@ -48,10 +61,17 @@ const ItemShow = () => {
 
             <div className="row mt-3">
                 <h3>Comments/Reviews</h3>
-                <div className="col-12">
-                    <b>Lester</b>
-                    <p>This is shit.</p>
-                </div>
+                <hr/>
+                {itemReviews &&
+                    itemReviews.map((itemReview, index) => {
+                        return (
+                            <div className="col-12" key={index}>
+                                <b>{itemReview[0].first_name}</b>
+                                <p>{itemReview[0].comment}</p>
+                            </div>
+                        )
+                    })
+                }
             </div>
         </div>
 
