d7ff159dfb5cdaf43bdac25c722ba9249f00205c
Lester063 <102047666+Lester063@users.noreply.github.com>
2024-11-07 00:04:00 +0800

    disabled button
    disabled button once Save review was clicked and the post succeed

--- src/components/FormInputs/ReviewForm.js
+++ src/components/FormInputs/ReviewForm.js
@@ -3,15 +3,16 @@ import { MyIdRequestContext } from "../../Pages/Returns/ReturnsList";
 import axios from 'axios';
 const ReviewForm = () => {
     const getidRequest = useContext(MyIdRequestContext);
+    const [buttonDisabled, setButtonDisabled] = useState(false);
     const [inputError, setInputError] = useState({});
-    const [review, setReview ] = useState({
+    const [review, setReview] = useState({
         rating: "",
         comment: "",
         idRequest: "",
     });
 
     useEffect(() => {
-        setReview({ ...review, idRequest:  getidRequest});
+        setReview({ ...review, idRequest: getidRequest });
     }, [getidRequest]);
 
     const submitReview = async (e) => {
@@ -22,11 +23,14 @@ const ReviewForm = () => {
             idRequest: review.idRequest,
         }
         try {
-            const response = await axios.post(`http://localhost:8000/api/comment`, data, { withCredentials: true });
+            const response = await axios.post(`http://localhost:8000/api/comment`,
+                data,
+                { withCredentials: true }
+            );
             alert(response.data.message);
-            console.log(response.data.data);
             setInputError({});
-        }catch(error) {
+            setButtonDisabled(true);
+        } catch (error) {
             console.log(error);
             setInputError(error.response.data.errors);
             console.log(error.response.data.errors);
@@ -40,15 +44,26 @@ const ReviewForm = () => {
     return (
         <>
             {review.idRequest &&
-                <form onSubmit = {submitReview}>
+                <form onSubmit={submitReview}>
                     <span className="text-danger">{inputError.message}</span>
-                    <input type="number" placeholder="Rating" className="form-control mt-1" name="rating" value={review.rating ? review.rating : 0} onChange = {handleChange} />
-                    <span className="text-danger">{inputError.rating}</span>
-                    <textarea placeholder="Comment" className="form-control mt-1" name="comment" value={review.comment} onChange = {handleChange} />
-                    <span className="text-danger">{inputError.comment}</span>
-                    <input type="hidden" className="form-control mt-1" value={review.idRequest} name="idRequest" onChange = {handleChange} />
 
-                    <button type="submit" className="btn btn-primary mt-1 float-end">Save review</button>
+                    <input type="number" max="5" min="0" placeholder="Rating"
+                        className="form-control mt-1" name="rating"
+                        value={review.rating ? review.rating : 0}
+                        onChange={handleChange} />
+                    <span className="text-danger">{inputError.rating}</span>
+
+                    <textarea placeholder="Comment" className="form-control mt-1"
+                        name="comment" value={review.comment}
+                        onChange={handleChange} />
+                    <span className="text-danger">{inputError.comment}</span>
+                    
+                    <input type="hidden" className="form-control mt-1"
+                        value={review.idRequest} name="idRequest"
+                        onChange={handleChange} />
+
+                    <button type="submit" className="btn btn-primary mt-1 float-end"
+                        disabled={buttonDisabled === false ? false : true}>Save review</button>
                 </form>
             }
 
